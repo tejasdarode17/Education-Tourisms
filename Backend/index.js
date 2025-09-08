67e000cfb9319fc8d31c43713290d98bccb9dfcf
Tejas Darode <tejasdarode17@gmail.com>
2025-09-08 17:07:22 +0530

    serving frontend from backned

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,20 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import dbConnect from "./config/dbConnect.js"
-import studentRoute from "./routes/studentRoute.js"
-import adminRoute from './routes/adminRoute.js'
-import dotenv from "dotenv"
-import path from 'path'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dbConnect from "./config/dbConnect.js";
+import studentRoute from "./routes/studentRoute.js";
+import adminRoute from './routes/adminRoute.js';
+import dotenv from "dotenv";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const allowedOrigins = [
     process.env.FRONTEND_URL,
@@ -34,17 +38,28 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-
+// API routes
 app.use("/api/v1", studentRoute);
 app.use("/api/v1", adminRoute);
 
+
+
+
+// Vite React frontend
+const frontendPath = path.join(__dirname, "../Frontend/dist");
+app.use(express.static(frontendPath, { index: false }));
+
+
+// SPA fallback
+app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+});
+
+//error handler
 app.use((err, req, res, next) => {
     console.error("Error:", err.message);
     res.status(500).json({
@@ -57,5 +72,3 @@ app.listen(PORT, () => {
     dbConnect();
     console.log(`Server running on port ${PORT}`);
 });
-
-
